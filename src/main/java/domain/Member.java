3de702a1d191cf6759e354ac38d9ae3a421d8ba3
c-zd <c-zd@gmail.com>
2025-02-01 19:55:18 +0900

    Memberの追加
    入力された内容を保持するクラス

---
+++ src/main/java/domain/Member.java
@@ -0,0 +1,15 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+	
+	private String name;
+	private String password;
+	
+}
